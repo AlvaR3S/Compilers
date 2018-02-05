196216a963ca347f5270e2ef3d694e1a2b14e55e
AlvaR3Z <reynaldo.alvarez1@marist.edu>
2018-02-05 02:03:42 -0500

    Added in the rest of the TokenTypes; Need to implement them in token array (matcher);

--- CustomCompiler/src/customcompiler/Lexer.java
+++ CustomCompiler/src/customcompiler/Lexer.java
@@ -20,16 +20,114 @@ public class Lexer {
     public static enum TokenType {
         
         
+        
+        // -----------------|End of Program Marker|---------------------
         EOP("[$]"),
         
+        
+        // -----------------|Keywords|Numbers|Letters|---------------------
+        // Keywords
+        type_int("[int]"),
+        type_String("[String]"),
+        type_boolean("[boolean]"),
+        type_abstract("[abstract]"),
+        type_continue("[continue]"),
+        type_for("[for]"),
+        type_new("[new]"),
+        type_switch("[switch]"),
+        type_assert("[assert]"),
+        type_default("[default]"),
+        type_goto("[goto]"),
+        type_package("[package]"),
+        type_synchronized("[synchronized]"),
+        type_do("[do]"),
+        type_if("[if]"),
+        type_private("[private]"),
+        type_this("[this]"),
+        type_break("[break]"),
+        type_double("[double]"),
+        type_implements("[implements]"),
+        type_protected("[protected]"),
+        type_throw("[throw]"),
+        type_byte("[byte]"),
+        type_else("[else]"),
+        type_import("[import]"),
+        type_public("[public]"),
+        type_throws("[throws]"),
+        type_case("[case]"),
+        type_enum("[enum]"),
+        type_instanceof("[instanceof]"),
+        type_return("[return]"),
+        type_transient("[transient]"),
+        type_catch("[catch]"),
+        type_extends("[extends]"),
+        type_short("[short]"),
+        type_try("[try]"),
+        type_char("[char]"),
+        type_final("[final]"),
+        type_interface("[interface]"),
+        type_static("[static]"),
+        type_void("[void]"),
+        type_class("[class]"),
+        type_finally("[finally]"),
+        type_long("[long]"),
+        type_strictfp("[strictfp]"),
+        type_volatile("[volatile]"),
+        type_const("[const]"),
+        type_float("[float]"),
+        type_native("[native]"),
+        type_super("[super]"),
+        type_while("[while]"),
+        
+        // Booleans
+        boolval_false("[false]"),
+        boolval_true("[true]"),
+        
+        // Chars
         CHAR("[a|b|c|d|e|f|g|h|i|j|k|l|"
                 + "m|n|o|p|q|r|s|t|u"
                 + "|v|w|x|y|z]"),
         
+        // Numbers
         digit("[1-9][0-9]*"), 
         
-        symbol("[*|/|+|-]"), 
         
+        // -----------------|Symbols|---------------------
+        // Assignment Operator
+        intop_assignment("[=]"),
+        
+        // Arithmetic Operators
+        intop_remainder("[%]"),
+        intop_substraction("[-]"),
+        intop_addition("[+]"),
+        intop_multiplication("[*]"), 
+        intop_division("[/]"),
+        
+        // Unary Operators
+        intop_increment("[++]"),
+        intop_decrement("[--]"),
+        
+        //Equality and Relational Operators
+        boolop_equalTo("[==]"),
+        boolop_notEqualTo("[!=]"),
+        boolop_greaterThan("[>]"),
+        boolop_greaterThanOrEqualTo("[>=]"),
+        boolop_lessThan("[<]"),
+        boolop_lessThanOrEqualTo("[<=]"),
+        
+        // Conditional Operators
+        boolop_AND("[&&]"),
+        boolop_OR("[||]"),
+        
+        // Brackets, Parenthesis, and Quotation Mark
+        opening_bracket("[{]"),
+        closing_bracket("[}]"),
+        opening_parenthesis("[(]"),
+        closing_parenthesis("[)]"),
+        //quote("[\" ]"),
+        
+        
+        // -----------------|Whitespace|---------------------
         space("[ \t\f\r\n]+");
         
         
@@ -76,16 +174,16 @@ public class Lexer {
                 tokens.add(new Token(TokenType.CHAR, matcher.group(TokenType.CHAR.name())));             
             } else if(matcher.group(TokenType.digit.name()) != null) {
                 tokens.add(new Token(TokenType.digit, matcher.group(TokenType.digit.name())));            
-            } else if(matcher.group(TokenType.symbol.name()) != null) {
-                tokens.add(new Token(TokenType.symbol, matcher.group(TokenType.symbol.name())));            
+            } else if(matcher.group(TokenType.intop_remainder.name()) != null) {
+                tokens.add(new Token(TokenType.intop_remainder, matcher.group(TokenType.intop_remainder.name())));            
             } else if(matcher.group(TokenType.EOP.name()) != null) {
-                tokens.add(new Token(TokenType.EOP, matcher.group(TokenType.EOP.name())));
-                
+                tokens.add(new Token(TokenType.EOP, matcher.group(TokenType.EOP.name()))); 
             }
         }
-    
+   
         return tokens;
     }
+    
     
     public static void main(String[] args) {
         int i = 1;
@@ -93,11 +191,9 @@ public class Lexer {
                 + "T > l >$"; //the input that will be tested
         
         boolean t = true;
-       
+        
         while(t = true) {
             
-             
-
             // Outputs a stream of tokens from the given input
             ArrayList<Token> tokens = lex(input);
             System.out.println("LEXER: Lexing program " + i + "...");
@@ -113,9 +209,9 @@ public class Lexer {
                     
                 }
             }
+             
             break;
-            
+           
         }  
-         
     }
 }
